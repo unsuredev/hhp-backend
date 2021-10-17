3fe41dd8965221eaaf065e9de06d2c6c52c20b8a
jamal <jamaluddin.mondal@mysmitch.com>
2021-10-17 13:00:17 +0530

    changed aws version and gitignore

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    transform: {'^.+\\.ts?$': 'ts-jest'},
-    testEnvironment: 'node',
-    testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    setupFiles:['./jest.setEnvVars'],
-    "collectCoverage": true,
-    "coverageReporters": ["text-summary", "html"]
-};
